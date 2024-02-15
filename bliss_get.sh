8627cf9e161f233968f5e42357861c1cbf3ce730
elppans <elppansmk@gmail.com>
2024-02-15 03:50:17 -0300

    Add files via upload

--- bliss_get.sh
+++ bliss_get.sh
@@ -4,5 +4,6 @@ export FILE="$1"
 export ADDR="https://sourceforge.net/projects/blissos-dev/files/Beta"
 
 curl -JOLk "$ADDR"/"$FILE" || \
-curl -C - -OLk  "$ADDR"/"$FILE" && \
+curl -C - -OLk  "$ADDR"/"$FILE"
+
 bsdtar -xvf "$FILE" initrd.img kernel system.sfs
