aec292141ef4c006e25475b61015a8ae60be6fae
elppans <elppansmk@gmail.com>
2024-02-15 13:49:22 -0300

    Delete bliss_get.sh

--- bliss_get.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export FILE="$1"
-export ADDR="https://sourceforge.net/projects/blissos-dev/files/Beta"
-
-curl -JOLk "$ADDR"/"$FILE" || \
-curl -C - -OLk  "$ADDR"/"$FILE"
-
-bsdtar -xvf "$FILE" initrd.img kernel system.sfs
